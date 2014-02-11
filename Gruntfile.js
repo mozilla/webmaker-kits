7b8d31895f4ebdf294ef3052ca2f1464d92790a6
William Duyck <fuzzyfox0@gmail.com>
2014-02-11 17:23:08 +0000

    add gruntfile + server

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		// running `grunt less` will compile once
+		less: {
+			development: {
+				options: {
+					paths: ["./teach-assets/css"],
+					yuicompress: true
+				},
+			files: {
+				"./teach-assets/css/style.css": "./teach-assets/less/style.less",
+				"./teach-assets/css/event.css": "./teach-assets/less/event.less",
+			}
+		}
+	},
+	// running `grunt watch` will watch for changes
+	watch: {
+		files: "./teach-assets/less/*.less",
+		tasks: ["less"]
+	}
+});
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
